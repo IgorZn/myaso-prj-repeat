2dcb94959b316dbcd323d4b1c7093ccf09b7c25a
Igor Zn <igor.znamensky@gmail.com>
2021-08-29 13:49:08 +0300

    095, 055

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,11 @@ const app = require('./app')
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || 'localhost';
+const URL = 'http://'
 
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT} port`)
+    console.log(`Server running on ${PORT} port, URL: ${URL}${HOST}:${PORT}`)
 })
 
 console.log(PORT)
