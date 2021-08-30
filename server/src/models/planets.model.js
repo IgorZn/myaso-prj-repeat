db441cd10407c9846a72a160d79d944fb0bc4b62
Igor Znamensky <igor.znamensky@gmail.com>
2021-08-30 10:21:47 +0300

    094, 057

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,7 +1,48 @@
-const parse = require('csv-parse');
 const fs = require('fs');
+const path = require('path')
+const parse = require('csv-parse');
+
+const dataFile = 'kepler_data.csv'
 
 const habitablePlanets = [];
+
+/*
+const promise = new Promise( (resolve, reject) => {
+    resolve(42)
+})
+promise.then((result) => {
+    console.log(result) # will be 42
+
+* */
+
+const fullPath = path.join('..', 'server','Data', dataFile)
+
+console.log(__dirname)
+console.log(fullPath)
+
+function loadPlanetsData() {
+    return new Promise( (resolve, reject) => {
+        fs.createReadStream(fullPath)
+            .pipe(parse({
+                comment: '#',
+                columns: true
+            }))
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+            .on('error', (err) => {
+                reject(err)
+            })
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} habitable planets found.`);
+                resolve('Done');
+            });
+
+    })
+
+};
 
 function isHabitablePlanet(planet){
     return planet['koi_disposition'] === 'CONFIRMED'
@@ -10,26 +51,8 @@ function isHabitablePlanet(planet){
         && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('../server/Data/kepler_data.csv')
-    .pipe(parse({
-        comment: '#',
-        columns: true
-    }))
-    .on('data', (data) => {
-        if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
-        }
-    })
-    .on('error', (err) => {
-        console.error(err)
-    })
-    .on('end', () => {
-        console.log(habitablePlanets.map((planet) => {
-            return planet['kepler_name']
-        } ) )
-        console.log(`${habitablePlanets.length} habitable planets found.`);
-    })
 
-const planets = [1,2];
-
-module.exports = planets;
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,19 @@
 const http = require('http')
 const app = require('./app')
+const { loadPlanetsData } = require('../src/models/planets.model')
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
 const URL = 'http://'
 
-server.listen(PORT, () => {
-    console.log(`Server running on ${PORT} port, URL: ${URL}${HOST}:${PORT}`)
-})
+async function startServer() {
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Server running on ${PORT} port, URL: ${URL}${HOST}:${PORT}`)
+    })
+}
+
+startServer();
 
 console.log(PORT)
