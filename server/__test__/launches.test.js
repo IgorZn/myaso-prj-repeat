c7c645c2badc2367551400bac33aa1c0ae0d2873
Igor Znamensky <igor.znamensky@gmail.com>
2021-09-09 21:29:12 +0300

    Add some tests.

--- server/__test__/launches.test.js
+++ server/__test__/launches.test.js
@@ -2,20 +2,69 @@ const request = require('supertest')
 const app = require('../src/app')
 
 describe('Test GET /launches', () => {
-    const response = request(app).get('/launches')
-    expect(response).toBe(200)
+
+    test('It should return 200 success', async () => {
+        await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
+
 });
 
 describe('Test POST /launch', () => {
-    test('It should respond with 200 succes', () => {
 
+    test('It should respond with 200 success', async () => {
+        const query = {
+            mission: 'ZTM+100500',
+            rocket: 'ZTM Explorer 1',
+            launchDate: new Date('December 27, 2030'),
+            target: 'Kepler-186 f'
+        };
+
+        await request(app)
+            .post('/launches')
+            .send(query)
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .then((res) => {
+                expect(res.body).toHaveProperty('upcoming', true)
+                expect(res.body).toHaveProperty('success', true)
+            })
     });
 
-    test('It should catch missing required properties', () => {
+    test('It should catch missing required properties', async () => {
+        const query = {
+            rocket: 'ZTM Explorer 1',
+            launchDate: new Date('December 27, 2030'),
+            target: 'Kepler-186 f'
+        };
 
+        await request(app)
+            .post('/launches')
+            .send(query)
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .then((res) => {
+                expect(res.body).toHaveProperty('error', 'Missing required launch property')
+            })
     });
 
-    test('It should catch invalid date', () => {
+    test('It should catch invalid date', async () => {
+        const query = {
+            mission: 'ZTM+100500',
+            rocket: 'ZTM Explorer 1',
+            launchDate: 'not Date',
+            target: 'Kepler-186 f'
+        };
 
+        await request(app)
+            .post('/launches')
+            .send(query)
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .then((res) => {
+                expect(res.body).toHaveProperty('error', 'Invalid launch date')
+            })
     })
 });
