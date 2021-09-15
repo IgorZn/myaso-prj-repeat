76d7a3d72547e836aac74246010aebb97f21f86a
Igor Zn <igor.znamensky@gmail.com>
2021-09-15 16:50:56 +0300

    142 - Launches Schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const mongoose = require('mongoose')
 const app = require('./app')
 const { loadPlanetsData } = require('../src/models/planets.model')
 
@@ -7,7 +8,22 @@ const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
 const URL = 'http://'
 
+
+const MONGO_URL = 'mongodb+srv://myaso_prj:QhdiCOhl6tFVHmBW@cluster0.xm9bg.mongodb.net/myaso_db?retryWrites=true&w=majority'
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready...')
+});
+
+mongoose.connection.on('error', err => {
+    console.error(err)
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Server running on ${PORT} port, URL: ${URL}${HOST}:${PORT}`)
